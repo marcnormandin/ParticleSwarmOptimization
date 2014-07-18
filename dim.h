6f303f3f658aa784d8d15ac85c12e0f1a0f4f8eb
Marc Normandin <normandin.utb@gmail.com>
2014-07-18 14:18:53 -0500

    Added missing dim.h file.

---
+++ dim.h
@@ -0,0 +1,42 @@
+/*
+ * dim.h
+ *
+ *  Created on: Jul 2, 2013
+ *      Author: marc
+ */
+
+#ifndef DIM_H_
+#define DIM_H_
+
+#include <cassert>
+
+typedef double dim_t;
+
+class Dim
+{
+public:
+    // Fixme
+    Dim() : mMin(1.0), mMax(2.0) {}
+
+    Dim(const dim_t mi, const dim_t ma)
+        : mMin(mi), mMax(ma)
+    {
+        assert(mMax >= mMin);
+    }
+
+    dim_t min() const
+    {
+        return mMin;
+    }
+    dim_t max() const
+    {
+        return mMax;
+    }
+
+private:
+    dim_t mMin;
+    dim_t mMax;
+};
+
+
+#endif /* DIM_H_ */
